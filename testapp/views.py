7956d5665aeb6dc4f6b7860c23a6b543424df394
tkr0218 <218fuji@gmail.com>
2024-10-05 19:21:33 +0900

    mod r

--- testapp/views.py
+++ testapp/views.py
@@ -2,6 +2,21 @@ from testapp import app
 from flask import render_template, request, redirect, url_for
 from testapp.models.recipes import recipe
 from testapp import db
+"""import requests
+import os
+class Image_DL():
+    def save_and_rename(self, url, name=None):
+        res = requests.get(url)
+        if res.status_code != 200:
+            return "No Image"
+        path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/media/image/"
+        if name==None:
+            path += url.split("/")[-1]
+        else:
+            path += name
+        with open(path, 'wb') as file:
+            file.write(res.content)
+        return path"""
 
 @app.route('/')
 def index():
@@ -20,10 +35,14 @@ def add_recipes():
     if request.method == 'GET':
         return render_template('testapp/add_recipes.html')
     if request.method == 'POST':
+        form_recipe = request.form.get('recipe')  # str
+        form_times = request.form.get('times')
+        form_diff = request.form.get('diff')
         reci = recipe(
-            name="カルボナーラ",
-            times="30分",
-            diff="2"
+            name=form_recipe,
+            times=form_times,
+            diff=form_diff,
+            #images=""
         )
         db.session.add(reci)
         db.session.commit()
